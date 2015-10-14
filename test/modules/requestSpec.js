ffc4b93629c883e40d8413bb67eb2f964bb235d8
ivebencrazy <ben@blanketwarriors.com>
2015-10-13 18:49:46 -0700

    [change] Changes request spec to check the first page for less test timeouts  - Only gets first page of results now  - Searches for /kanai-kun/ rather than /shokugeki-no-souma/

--- test/modules/requestSpec.js
+++ test/modules/requestSpec.js
@@ -4,19 +4,19 @@ import requestUtilities from '../../app/modules/requestUtilities.js';
 describe('Request Utilities', function() {
 
   it('should get the manga list with callback javascript', function(done) {
-    requestUtilities.getData({img: false, data: 'api/list/0/'}, function(error, response) {
+    requestUtilities.getData({img: false, data: 'api/list/0/?p=0'}, function(error, response) {
       expect(error).toBeNull();
       expect(typeof response).toBe('object');
-      expect(JSON.stringify(response)).toMatch(/shokugeki-no-soma/);
+      expect(JSON.stringify(response)).toMatch(/kanai-kun/);
       done();
     });
   });
 
   it('should get the manga list with reactive javascript', function(done) {
-    requestUtilities.getData$({img: false, data: 'api/list/0/'}).forEach({
+    requestUtilities.getData$({img: false, data: 'api/list/0/?p=0'}).forEach({
       onNext: response => {
         expect(typeof response).toBe('object');
-        expect(JSON.stringify(response)).toMatch(/shokugeki-no-soma/);
+        expect(JSON.stringify(response)).toMatch(/kanai-kun/);
       },
       onError: error => console.error(error),
       onCompleted: () => done()
